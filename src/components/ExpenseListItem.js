dc09413e8ce00729642b33be9060375b6c68e162
edom <edom.zhang@gmail.com>
2019-10-26 22:22:17 +1100

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { removeExpense } from "../actions/expenses";
 import moment from "moment";
+import numeral from "numeral";
 import { Link } from "react-router-dom";
 
 // Using destructuring
@@ -17,7 +18,8 @@ export const ExpenseListItem = ({
       <h3>{description}</h3>
     </Link>
     <p>
-      {amount} - {moment(createdAt).format("MMM Do, YYYY")}
+      {numeral(amount / 100).format("$0,0.00")} -{" "}
+      {moment(createdAt).format("MMMM Do, YYYY")}
     </p>
   </div>
 );
