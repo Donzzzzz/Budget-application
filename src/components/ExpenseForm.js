026ced2a1bcf5ab40fa20ceb2c3267ccf6085de0
edom <edom.zhang@gmail.com>
2019-11-14 17:20:03 +1100

    Styling the list filters

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -69,6 +69,7 @@ export default class ExpenseForm extends React.Component {
         {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.onSubmit}>
           <input
+            className="text-input"
             type="text"
             placeholder="Description"
             autoFocus
@@ -76,6 +77,7 @@ export default class ExpenseForm extends React.Component {
             onChange={this.onDescriptionChange}
           />
           <input
+            className="text-input"
             type="text"
             placeholder="Amount"
             value={this.state.amount}
@@ -91,6 +93,7 @@ export default class ExpenseForm extends React.Component {
             isOutsideRange={() => false}
           />
           <textarea
+            className="textarea"
             placeholder="Add a note for your expense(optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
